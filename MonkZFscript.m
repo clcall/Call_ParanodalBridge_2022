ed9929a0d49340d0420116dbf9f538735fefe798
Cody Call <61843069+clcall@users.noreply.github.com>
2023-06-16 17:02:47 -0700

    owen data, loop analysis

--- MonkZFscript.m
+++ MonkZFscript.m
@@ -1,13 +1,21 @@
 cd('D:\GitHubRepos\Call_ParanodalBridge_2022\MonkZFtraces');
+
+xmlstruct = parseXML_SingleCell('fish07_cellE_bridgedSheath2_230508.traces');
+% TO DO:
 % file loop
-xmlstruct = parseXML_SingleCell('fish08_cellO_nonbridgedSheath1_230503_no16.traces');
 % save struct
 %     varname = matlab.lang.makeValidName(contents(k).name);
 %     save(fullfile(directory,aClass,[varname '.mat']),'xmlstruct');
+%
+% convert length back to microns for wrap quant
+%
+% convert tp to real time
+%
+% note locations of bridges per tp
 %%
 useImaginaryEdge = 0;
 makeplot = 1;
-[d,X,Delta,borders,origin] = calculatePathsXML_ZFbridges(xmlstruct,useImaginaryEdge,makeplot);
+[d,X,Delta,borders,origin,firstbrgtp] = calculatePathsXML_ZFbridges(xmlstruct,useImaginaryEdge,makeplot);
 
 %%
 smol = min(cell2mat(cellfun(@min,cellfun(@min,Delta,'UniformOutput',false),'UniformOutput',false)));
@@ -21,21 +29,8 @@ for i = 1:length(Delta)
         Dall = [Dall;Dadj];
     end
 end
-% add extra blank rows to be able to get peaks/troughs in first and last rows
-Dall = [zeros(1,size(Dall,2));Dall;zeros(1,size(Dall,2))];
+Dall = [zeros(1,size(Dall,2));Dall;zeros(1,size(Dall,2))]; % adds extra blank rows to be able to get peaks/troughs in first and last rows
 
-%%
-% smol = min(cell2mat(cellfun(@min,cellfun(@min,X,'UniformOutput',false),'UniformOutput',false)));
-% big = max(cell2mat(cellfun(@max,cellfun(@max,X,'UniformOutput',false),'UniformOutput',false)));
-% rng = smol:big;
-% drawadj = NaN(size(rng));
-% drawall = [];
-% for i =1:length(d)
-%     if any(d{i})
-%         drawadj(ismember(X{i},rng)) = d{i}(:,1);
-%         drawall = [drawall; drawadj];
-%     end
-% end
 %%
 lmin = islocalmin(Dall,1,"MinProminence",0.5);
 lmax = islocalmax(Dall,1,"MinProminence",0.5);
@@ -52,6 +47,7 @@ figQuality(gcf,gca,[11,2],1)
 
 figure; heatmap(Dall(2:end-1,:),'Colormap',parula,'ColorLimits',[-1.5,1.5]); grid off
 figQuality(gcf,gca,[11,2],1)
+
 %% calculate extension and keep track of wraps up to then when extended
 wraps = zeros(1,size(Dall,2));
 for i = 1:size(Dall,1)-1
@@ -65,10 +61,10 @@ for i = 1:size(Dall,1)-1
     temp = find(~isnan(Dall(i-1,:)));
     edges_0i = [temp(1),temp(end)];
     if edges_i(1) < edges_0i(1)
-        wraps(edges_i(1):edges_0i(1)) = wraps(edges_i(1):edges_0i(1)) + repmat(mode(wraps(edges_0i(1)+20)),[1,edges_0i(1)-edges_i(1)+1]);
+        wraps(edges_i(1):edges_0i(1)) = wraps(edges_i(1):edges_0i(1)) + repmat(mode(wraps(edges_0i(1)+50)),[1,edges_0i(1)-edges_i(1)+1]);
     end
     if edges_i(end) > edges_0i(end)
-        wraps(edges_0i(end):edges_i(end)) = wraps(edges_0i(end):edges_i(end)) + repmat(mode(wraps(edges_0i(end)-20)),[1,edges_i(end)-edges_0i(end)+1]);
+        wraps(edges_0i(end):edges_i(end)) = wraps(edges_0i(end):edges_i(end)) + repmat(mode(wraps(edges_0i(end)-50)),[1,edges_i(end)-edges_0i(end)+1]);
     end
     
     % get new peaks/troughs
@@ -84,6 +80,28 @@ for i = 1:size(Dall,1)-1
     wraps(idx_down) = wraps(idx_down) + 0.5;
     wraps(idx_up) = wraps(idx_up) + 0.5;
     
+    switch i
+        case firstbrgtp-1
+            figure 
+            plot(wraps)
+            title('TP(first bridge) - 1')
+            hold on
+            plot(smoothdata(wraps,'movmedian',50))
+            hold off
+            figQuality(gcf,gca,[11,2])
+            box off
+            ylim([0,3])
+        case firstbrgtp+5 % note this is arbitrary, still need to convert tp to real time
+            figure 
+            plot(wraps)
+            title('TP(first bridge) + 5')
+            hold on
+            plot(smoothdata(wraps,'movmedian',50))
+            hold off
+            figQuality(gcf,gca,[11,2])
+            box off
+            ylim([0,3])
+    end
 end
 figure 
 plot(wraps)
--- calculatePathsXML_ZFbridges.m
+++ calculatePathsXML_ZFbridges.m
@@ -1,4 +1,4 @@
-function [d,X,Delta,borders,origin] = calculatePathsXML_ZFbridges( xmlstruct, useImaginaryEdge, makeplot)
+function [d,X,Delta,borders,origin,firstbrgtp] = calculatePathsXML_ZFbridges( xmlstruct, useImaginaryEdge, makeplot)
 [traceData_sheaths, traceData_lifeact] = parseData(xmlstruct);
 frames_sheaths = cell2mat(traceData_sheaths(:,2));
 frames_lifeact = cell2mat(traceData_lifeact(:,2));
@@ -7,6 +7,7 @@ X = cell(1,max(frames_sheaths));
 Delta = cell(1,max(frames_sheaths));
 f1 = figure;
 %f2 = figure;
+firstbrg = 0;
 for i=1:max(frames_sheaths)
     temp = traceData_sheaths(frames_sheaths==i,:);
     if isempty(temp)
@@ -34,6 +35,10 @@ for i=1:max(frames_sheaths)
         end
         if cell2mat(temp(I(k),5)) == 1 % is a bridge
             b=1;
+            if ~firstbrg
+                firstbrgtp = i;
+                firstbrg = 1;
+            end
             % make straight line always on same edge as sheath traces
             if nargin>1 && useImaginaryEdge
                 allpts = cell2mat(temp(:,6));
@@ -73,45 +78,11 @@ for i=1:max(frames_sheaths)
     
     %% PLOT RECENT CHANGE
     if i>1 && any(X{i-1})
-%         figure(f2);
-%         hold on
-%         subplot(max(frames_sheaths),1,i)
         idx1 = ismember(X{i-1},X{i}); % length of i-1
         idx2 = ismember(X{i},X{i-1}); % length of i
         delta = d{i}(idx2,1)-d{i-1}(idx1,1);
-%         h2 = plot(X{i-1}(idx1),delta,'LineWidth',2);
         Delta{i} = [X{i-1}(idx1)',delta];
-        %             map = zeros(length(delta),3);
-        %             map(delta<0,:) = repmat([0,255,255],size(map(delta<0,1)));
-        %             map(delta>0,:) = repmat([255,0,255],size(map(delta>0,1)));
-        %             map(:,4) = 255;
-        %             map = uint8(map');
-        %             drawnow
-        %             set(h2.Edge,'ColorBinding','interpolated','ColorData',map)
-%         cdo = colormap('parula');
-%         cdo(74:128+54,:) = repmat([0.5,0.5,0.5],[109,1]);
-%         cd = interp1(linspace(-1.2,1.2,length(cdo)),cdo,delta);
-%         cd = uint8(cd'*255);
-%         logdx = all(cd==0);
-%         cd(4,:) = 255;
-%         drawnow
-%         set(h2.Edge,'ColorBinding','interpolated','ColorData',cd)
-%         
-%         yline(0)
-%         ylim([-1.5,1.5])
-%         xlim([-1500,1500])
-%         xticks([])
-%         xticklabels([])
-%         axis off
-%         box off
-%         
-%         if any(borders)
-%             xline(borders,'Linewidth',1.5);
-%         end
-%         
-%         hold off
     end
-    
     %% PLOT ABSOLUTE DISTANCE
     if makeplot
         figure(f1);
@@ -119,7 +90,7 @@ for i=1:max(frames_sheaths)
         subplot(max(frames_sheaths),1,i)
         %         h = plot(X,ones(size(d,1),1),'LineWidth',2);
         h = plot(X{i},d{i}(:,1),'LineWidth',2);
-
+        
         cdo = colormap('parula');
         cd = interp1(linspace(0,1.2,length(cdo)),cdo,d{i}(:,1));
         cd = uint8(cd'*255);
@@ -132,14 +103,14 @@ for i=1:max(frames_sheaths)
         cd(4,:) = 255;
         drawnow
         set(h.Edge,'ColorBinding','interpolated','ColorData',cd)
-
+        
         ylim([0,2])
         xlim([-1500,1500])
         xticks([])
         xticklabels([])
         axis off
         box off
-
+        
         if any(borders)
             xline(borders,'Linewidth',1.5);
         end
@@ -147,104 +118,58 @@ for i=1:max(frames_sheaths)
         hold off
     end
 end
-
-
-%     traceNames = traceData_sheaths(:,1);
-%     traceMatData = cell2mat(traceData_sheaths(:,2:6));
-%          if sum(cellfun(@isempty,traceData_sheaths(:,2))) > 0
-%              warning('Format error. Something is mistyped.')
-%              return
-%          end
-%     axon_num = max(traceMatData(:,1)); % pulls out the number of axons
-%     totalAxonLength = [];
-%     totalSheathLength = [];
-%     avgInternodeLength = [];
-%     axonName = [];
-%     allInternodes = [];
-%
-%     for i = 1:axon_num
-%         axon_idx = find(traceMatData(:,1)==i & traceMatData(:,2)==0);
-%         temp = traceMatData(axon_idx,:);
-%         tempStr = traceNames(axon_idx);
-%         if isempty(tempStr)
-%            continue
-%         end
-%         tempStr = tempStr(1);
-%         totalAxonLength = [totalAxonLength; sum(temp(:,3))];
-%         axonName = [axonName; tempStr];
-%
-%         %TOTAL SHEATH LENGTH
-%         sheath_idx = traceMatData(:,1)==i & traceMatData(:,2)~=0;
-%         internodes = traceMatData(sheath_idx,:);
-%         totalSheathLength = [totalSheathLength; sum(internodes(:,3))];
-%
-%         %AVG INTERNODE LENGTH
-%         if any(traceMatData(:,5))
-%             uncut_ints = internodes(internodes(:,5)~=1,:);
-%             avgInternodeLength = [avgInternodeLength; mean(uncut_ints(:,3))];
-%             allInternodes = [allInternodes; uncut_ints(:,1:3)];
-%         else
-%             if(size(internodes,1) >= 3)
-%                 avgInternodeLength = [avgInternodeLength; mean(internodes(2:end-1,3))];
-%             else
-%                 avgInternodeLength = [avgInternodeLength; NaN];
-%             end
-%         end
-%     end
-%     percentMyelin = (totalSheathLength ./ totalAxonLength)  * 100;
-%     myelinPercent = table(axonName,avgInternodeLength,totalAxonLength,totalSheathLength,percentMyelin);
 end
 
-%----Local Function----
+%% ----Local Function----
 function [ parsedData_sheaths, parsedData_lifeact ] = parseData(xmlstruct)
-    %get length of paths list
-    numPaths = size(xmlstruct.paths,2);
-    traceName = cell(numPaths,1);
-    traceLength = cell(numPaths,1);
-    traceSWC = cell(numPaths,1);
-    traceColor = cell(numPaths,1);
-    traceFrame = cell(numPaths,1);
-    traceChannel = cell(numPaths,1);
-    tracePaths = cell(numPaths,1);
-    for i = 1:numPaths
-        traceName{i,1} = xmlstruct.paths(i).attribs.name;
-        traceLength{i,1} = xmlstruct.paths(i).attribs.reallength_smoothed;
-        traceSWC{i,1} = str2double(xmlstruct.paths(i).attribs.swctype);
-        if contains(xmlstruct.paths(i).attribs.color,'Orange')
-            traceColor{i,1} = 1; % meaning bridge
-        elseif contains(xmlstruct.paths(i).attribs.color,'Red')
-            traceColor{i,1} = 2; % meaning sheath frag with first point = process intersection (sheath origin)
-        else
-            traceColor{i,1} = 0;
-        end
-        traceFrame{i,1} = str2double(xmlstruct.paths(i).attribs.frame);
-        traceChannel{i,1} = xmlstruct.paths(i).attribs.channel;
-        tracePaths{i,1} = xmlstruct.paths(i).points.smoothed;
+%get length of paths list
+numPaths = size(xmlstruct.paths,2);
+traceName = cell(numPaths,1);
+traceLength = cell(numPaths,1);
+traceSWC = cell(numPaths,1);
+traceColor = cell(numPaths,1);
+traceFrame = cell(numPaths,1);
+traceChannel = cell(numPaths,1);
+tracePaths = cell(numPaths,1);
+for i = 1:numPaths
+    traceName{i,1} = xmlstruct.paths(i).attribs.name;
+    traceLength{i,1} = xmlstruct.paths(i).attribs.reallength_smoothed;
+    traceSWC{i,1} = str2double(xmlstruct.paths(i).attribs.swctype);
+    if contains(xmlstruct.paths(i).attribs.color,'Orange')
+        traceColor{i,1} = 1; % meaning bridge
+    elseif contains(xmlstruct.paths(i).attribs.color,'Red')
+        traceColor{i,1} = 2; % meaning sheath frag with first point = process intersection (sheath origin)
+    else
+        traceColor{i,1} = 0;
     end
-    %PARSE TRACE INFO
-    %sheath extraction
-    index = find(contains(traceChannel,'1')); % Ch1 - binarized myrEGFP, Ch2 - myrEGFP, Ch3 - lifeact
-    Names = traceName(index);
-    Lengths = traceLength(index);
-    SWCs = traceSWC(index);
-    Colors = traceColor(index);
-    Paths = tracePaths(index);
-    Channels = traceChannel(index);
-    Frames = traceFrame(index);
+    traceFrame{i,1} = str2double(xmlstruct.paths(i).attribs.frame);
+    traceChannel{i,1} = xmlstruct.paths(i).attribs.channel;
+    tracePaths{i,1} = xmlstruct.paths(i).points.smoothed;
+end
+%PARSE TRACE INFO
+%sheath extraction
+index = find(contains(traceChannel,'1')); % Ch1 - binarized myrEGFP, Ch2 - myrEGFP, Ch3 - lifeact
+Names = traceName(index);
+Lengths = traceLength(index);
+SWCs = traceSWC(index);
+Colors = traceColor(index);
+Paths = tracePaths(index);
+Channels = traceChannel(index);
+Frames = traceFrame(index);
 
-    %create array to send main function name & length info
-    parsedData_sheaths = [Names, Frames, Lengths, SWCs, Colors, Paths];
+%create array to send main function name & length info
+parsedData_sheaths = [Names, Frames, Lengths, SWCs, Colors, Paths];
 
-    index = find(contains(traceChannel,'3')); % Ch1 - binarized myrEGFP, Ch2 - myrEGFP, Ch3 - lifeact
-    Names = traceName(index);
-    Lengths = traceLength(index);
-    SWCs = traceSWC(index);
-    Colors = traceColor(index);
-    Paths = tracePaths(index);
-    Channels = traceChannel(index);
-    Frames = traceFrame(index);
+index = find(contains(traceChannel,'3')); % Ch1 - binarized myrEGFP, Ch2 - myrEGFP, Ch3 - lifeact
+Names = traceName(index);
+Lengths = traceLength(index);
+SWCs = traceSWC(index);
+Colors = traceColor(index);
+Paths = tracePaths(index);
+Channels = traceChannel(index);
+Frames = traceFrame(index);
 
-    %create array to send main function name & length info
-    parsedData_lifeact = [Names, Frames, Lengths, SWCs, Colors, Paths];
+%create array to send main function name & length info
+parsedData_lifeact = [Names, Frames, Lengths, SWCs, Colors, Paths];
 
 end
